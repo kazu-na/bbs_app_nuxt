948817ea0b350b5557860f6f97ac713bf312246e
kazu_na <kazuhide.n428@gmail.com>
2020-02-24 01:14:04 +0900

    bbs_app フロントエンド nuxt

---
+++ components/BoardList.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title>
+      掲示板
+      <v-spacer></v-spacer>
+      <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="boards" :search="search"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["boards"],
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "タイトル",
+          align: "left",
+          sortable: false,
+          value: "title"
+        },
+        { text: "ユーザー名", value: "username" },
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <AddBoard @submit="addBoard" />
+    <BoardList :boards="boards" />
+  </div>
+</template>
+
+<script>
+import AddBoard from "@/components/AddBoard";
+import BoardList from "@/components/BoardList";
+
+export default {
+  components: {
+    AddBoard,
+    BoardList
+  },
+  data(){
+    return {
+      boards: []
+    }
+  },
+  methods: {
+    addBoard(title) {
+      this.boards.push({
+        title
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
